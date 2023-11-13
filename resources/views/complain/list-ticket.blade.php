07a9547d561315097750ea3a94779b85086024c2
marcolcu <116879840+marcolcuu@users.noreply.github.com>
2023-11-13 22:03:27 +0700

    [UPDATE] Memperbaiki tampilan list invoice dan list ticket

--- resources/views/complain/list-ticket.blade.php
+++ resources/views/complain/list-ticket.blade.php
@@ -19,7 +19,36 @@
         </ol>
     </nav>
 
-    <div class="bg-white rounded-3">
+    {{-- Card --}}
+    <div class="card mb-4">
+        <div class="card-widget-separator-wrapper">
+            <div class="card-body card-widget-separator">
+                <div class="row gy-4 gy-sm-1">
+                    <div class="col-sm-6 col-lg-6">
+                        <div class="d-flex justify-content-center align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                            <div class="text-center">
+                                <h3 class="mb-1">300</h3>
+                                <p class="mb-0">Tenant</p>
+                            </div>
+                        </div>
+                        <hr class="d-none d-sm-block d-lg-none me-4">
+                    </div>
+                    <div class="col-sm-6 col-lg-6">
+                        <div class="d-flex justify-content-center align-items-start card-widget-2 pb-3 pb-sm-0">
+                            <div class="text-center">
+                                <h3 class="mb-1">50</h3>
+                                <p class="mb-0">Invoice</p>
+                            </div>
+                        </div>
+                        <hr class="d-none d-sm-block d-lg-none">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Datatables --}}
+    <div class="card">
         <div class="card-datatable table-responsive pt-0">
             <table class="datatables-basic table">
                 <thead>
--- resources/views/invoice/list-invoice.blade.php
+++ resources/views/invoice/list-invoice.blade.php
@@ -19,7 +19,52 @@
         </ol>
     </nav>
 
-    <div class="bg-white rounded-3">
+    {{-- Card --}}
+    <div class="card mb-4">
+        <div class="card-widget-separator-wrapper">
+            <div class="card-body card-widget-separator">
+                <div class="row gy-4 gy-sm-1">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                            <div>
+                                <h3 class="mb-1">300</h3>
+                                <p class="mb-0">Tenant</p>
+                            </div>
+                        </div>
+                        <hr class="d-none d-sm-block d-lg-none me-4">
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="d-flex justify-content-between align-items-start card-widget-2 border-end pb-3 pb-sm-0">
+                            <div>
+                                <h3 class="mb-1">100</h3>
+                                <p class="mb-0">Invoice</p>
+                            </div>
+                        </div>
+                        <hr class="d-none d-sm-block d-lg-none">
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="d-flex justify-content-between align-items-start border-end pb-3 pb-sm-0 card-widget-3">
+                            <div>
+                                <h3 class="mb-1">Rp. 20.000.000</h3>
+                                <p class="mb-0">Terbayarkan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="d-flex justify-content-between align-items-start pb-3 pb-sm-0 card-widget-3">
+                            <div>
+                                <h3 class="mb-1">Rp. 5.000.000</h3>
+                                <p class="mb-0">Belum Dibayarkan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Datatables --}}
+    <div class="card">
         <div class="card-datatable table-responsive pt-0">
             <table class="datatables-basic table">
                 <thead>
