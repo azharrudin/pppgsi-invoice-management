d4cc01d79b115d2c0263561ca9cac9cff559a765
Eric <eric658932@gmail.com>
2023-11-30 20:18:56 +0700

    add Vendor
    Membuat model, migration, dan seeder vendor
    
    Membuat CRUD Vendor
    
    Membuat VendorService

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ReceiptController;
 use App\Http\Controllers\TenantController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\VendorController;
 use App\Http\Controllers\WorkOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -41,3 +42,5 @@ Route::get("damage-report/select", [DamageReportController::class, "select"]);
 Route::resource('damage-report', DamageReportController::class);
 
 Route::resource('work-order', WorkOrderController::class);
+
+Route::resource('vendor', VendorController::class);
