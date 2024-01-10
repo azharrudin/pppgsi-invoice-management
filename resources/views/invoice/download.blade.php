c4bbad5a1691f47a5c51a025f06e3072429e956f
ArdiDwiWibowo07 <ardw.wibowo@gmail.com>
2024-01-10 12:56:05 +0700

    [Updated] print

--- resources/views/invoice/download.blade.php
+++ resources/views/invoice/download.blade.php
@@ -1,3 +1,35 @@
+@php
+function tgl_indo($tanggal){
+$bulan = array (
+1 => 'Januari',
+'Februari',
+'Maret',
+'April',
+'Mei',
+'Juni',
+'Juli',
+'Agustus',
+'September',
+'Oktober',
+'November',
+'Desember'
+);
+$pecahkan = explode('-', $tanggal);
+
+// variabel pecahkan 0 = tanggal
+// variabel pecahkan 1 = bulan
+// variabel pecahkan 2 = tahun
+
+return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+}
+
+function rupiah($angka){
+
+$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+return $hasil_rupiah;
+
+}
+@endphp
 <!doctype html>
 <html lang="en">
 
@@ -68,20 +100,17 @@
                         <td rowspan="3" style="border: none; width: 100px;"><b>Kepada
                                 Yth:
                             </b></td>
-                        <td rowspan="3" style="border: 0.5px solid black;"><b>PT. Focus
-                                Media
-                                Indonesia The Capitol
-                                Building Lt 1
-                                Jl. Letjen S. Parman Kav. 73 Slipi Jakarta
-                                Barat
-                                <br><br>
-                                Up. Bp. Chrissandy Dave Winata</b></td>
+                        <td rowspan="3" style="border: 0.5px solid black;"><b>
+                                {{$data->tenant->company}} <br>
+                                {{$data->tenant->floor}} <br>
+                                <br>
+                                Up. {{$data->tenant->name}}</b></td>
                         <td style="width: 120px; border: none;"></td>
                         <td style="border: 0.5px solid black;">No. Invoice:
                             <br>{{$data->invoice_number}}
                         </td>
                         <td style="border: 0.5px solid black;">Tanggal: <br>
-                            {{$data->invoice_date}}
+                            {{tgl_indo($data->invoice_date)}}
                         </td>
                     </tr>
                     <tr>
@@ -90,7 +119,7 @@
                         <td style="border: 0.5px solid black;">No. Kontrak:
                             <br> {{$data->contract_number}}
                         </td>
-                        <td style="border: 0.5px solid black;">Tanggal: <br> {{$data->contract_date}}
+                        <td style="border: 0.5px solid black;">Tanggal: <br> {{tgl_indo($data->contract_date)}}
                         </td>
                     </tr>
                     <tr>
@@ -99,8 +128,8 @@
                         <td style="border: 0.5px solid black;">No. Addendum:
                             <br> {{$data->addendum_number}}
                         </td>
-                        <td style="border: 0.5px solid black;">Tanggal: <br>{{$data->addendum_date}}
-                        </td> 
+                        <td style="border: 0.5px solid black;">Tanggal: <br>{{tgl_indo($data->addendum_date)}}
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -117,53 +146,53 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach($data->invoice_details as $p)
                     <tr>
-                        <td>Sewa Penempatan Perangkat LED</td>
-                        <td>Periode: 01 Maret 2021 s/d 31 Maret 2021</td>
-                        <td>9.200.181</td>
-                        <td>1.012.019.91</td>
-                        <td>10.212.201</td>
+                        <td>{{$p->item}}</td>
+                        <td>{{$p->description}}</td>
+                        <td>{{rupiah($p->price)}}</td>
+                        <td>{{$p->tax->rate}}%</td>
+                        <td>{{rupiah($p->total_price)}}</td>
                     </tr>
-                    <tr>
-                        <td>Biaya Material</td>
-                        <td></td>
-                        <td>10.000</td>
-                        <td>0</td>
-                        <td>1.022.020</td>
-                    </tr>
+                    @endforeach
                 </tbody>
                 <tfoot>
                     <tr>
                         <td class="text-right" colspan="4"><b>Total :</b>
                         </td>
-                        <td>{{$data->grand_total}}</td>
+                        <td>{{rupiah($data->grand_total)}}</td>
                     </tr>
                     <tr>
                         <td colspan="5"><b>Terbilang: {{$data->grand_total_spelled}}</b></td>
                     </tr>
                 </tfoot>
             </table>
-            <p>Jatuh Tempo Tgl : {{$data->invoice_due_date}}</p><br>
+            <p>Jatuh Tempo Tgl : {{tgl_indo($data->invoice_due_date)}}</p><br>
             <p>Keterangan : {{$data->term_and_conditions}}</p><br>
             <p></p>
         </div>
         <div class="row">
             <div class="col-4" style="border: 1px solid black;">
-                Pembayaran dengan Cek/Bilyet/Transfer atas nama:
-                PPPGSI
-                BANK MANDIRI
-                CABANG JAKARTA KRAKATAU STEEL Account No.: 070-000.713846-9
+                Pembayaran dengan Cek/Bilyet/Transfer atas nama: <br />
+                {{$data->bank->account_name}} <br />
+                {{$data->bank->name}} <br />
+                CABANG {{$data->bank->branch_name}} <br />
+                Account No. : {{$data->bank->account_number}}
             </div>
             <div class="col-4"></div>
             <div class="col-4">
-
+                <br>
+                <br>
                 <div class="ttd" style="width: max-content; float: right;">
 
-                    <p style="display: block; text-align: center; padding: 0; margin: 0;">Jakarta,
-                        7 September 2023<br><br><br><br><br><b><u>Zulfikar
-                                A.
-                                R.</u></b><br><span>Ka.
-                            BM</span></p>
+                    <p style="display: block; text-align: center; padding: 0; margin: 0;">Jakarta, {{tgl_indo($data->materai_date)}}<br>
+                        <img src="{{$data->materai_image}}" alt="">
+                    <p class="text-center">
+                        <u>{{$data->materai_name}}</u></b><br><span>Ka.
+                            BM</span>
+                    </p>
+                    </p>
+
                 </div>
             </div>
             <div style="clear: both;"></div>
