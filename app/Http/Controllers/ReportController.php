e84faec7aef327bc219931159357986b9fa8f10a
Eric <eric658932@gmail.com>
2024-02-10 18:54:10 +0700

    update ReportController
    Memperbaiki endpoint report/dashboard agar mengembalikan data yang benar

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -37,33 +37,50 @@ class ReportController extends Controller
                 $end->setTime(23, 59, 59);
             }
 
-            $countTenant = Tenant::where("deleted_at", null)->whereBetween("created_at", [$start, $end])->count();
-            $countInvoice = Invoice::where("deleted_at", null)->whereBetween("created_at", [$start, $end])->count();
-            $countInvoiceExpired = Invoice::where("deleted_at", null)->
-                where("invoice_due_date", "<=", $start)->
-                where("status", '!=', 'Lunas')->
-                count();
-            $countInvoicePaid = Invoice::where("deleted_at", null)->
-                whereBetween("created_at", [$start, $end])->
-                where("status", 'like', '%Lunas%')->
-                count();
-            $countInvoiceNotPaid = Invoice::where("deleted_at", null)->
-                whereBetween("created_at", [$start, $end])->
-                where("status", '!=', 'Lunas')->
-                count();
-            $countVendorInvoice = PurchaseOrder::with("vendor")->
-                whereBetween("created_at", [$start, $end])->
-                where("deleted_at", null)->
-                where('status', 'like', '%disetujui bm%')->
-                count();
+            $sumInvoicePerMonth = Invoice::select(
+                DB::raw('MONTH(created_at) AS month'),
+                DB::raw('YEAR(created_at) AS year'),
+                DB::raw('SUM(grand_total) AS total_amount')
+            )
+            ->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))
+            ->where(DB::raw('YEAR(created_at)'), "like", Carbon::now()->year)
+            ->get();
+
+            $incomeReportQuery = "
+                SELECT
+                    (SELECT sum(grand_total) FROM invoices WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end' AND status LIKE '%Lunas%') AS sum_invoices,
+                    (SELECT COUNT(*) FROM invoices WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end') AS count_invoices,
+                    (SELECT COUNT(*) FROM invoices WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end' AND status LIKE '%Lunas%') AS count_invoices_paid,
+                    (SELECT COUNT(*) FROM invoices WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end' AND status != 'Lunas') AS count_invoices_not_paid
+            ";
+            $incomeReport = DB::select($incomeReportQuery)[0];
+            $incomeReport->sum_invoice_per_month = $sumInvoicePerMonth;
+
+            $ticketComplainQuery = "
+                SELECT
+                    (SELECT COUNT(*) FROM tickets WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end') AS count_tickets,
+                    (SELECT COUNT(*) FROM tickets WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end' AND status LIKE '%wait a response%') AS count_tickets_waiting_for_response,
+                    (SELECT COUNT(*) FROM tickets WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end' AND status LIKE '%selesai%') AS count_completed_tickets
+            ";
+            $ticketComplain = DB::select($ticketComplainQuery)[0];
+
+            $tableArr = ["work_orders", "material_requests", "purchase_requests", "purchase_orders"];
+            $countDataQueryString = "SELECT ";
+
+            foreach($tableArr as $tableName){
+                $countQuery = "(SELECT COUNT(*) FROM $tableName WHERE deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end') AS count_$tableName, ";
+
+                $countDataQueryString = $countDataQueryString . $countQuery;
+            }
+
+            $countDataQueryString = $countDataQueryString . "(SELECT COUNT(*) FROM purchase_orders WHERE status LIKE '%disetujui bm%' AND deleted_at IS NULL AND created_at BETWEEN '$start' AND '$end') AS count_vendor_invoice";
+
+            $countData = DB::select($countDataQueryString)[0];
 
             return [
-                "count_tenant" => $countTenant,
-                "count_invoice" => $countInvoice,
-                "count_invoice_expired" => $countInvoiceExpired,
-                "count_invoice_paid" => $countInvoicePaid,
-                "count_invoice_not_paid" => $countInvoiceNotPaid,
-                "count_vendor_invoice" => $countVendorInvoice,
+                "income_report" => $incomeReport,
+                "ticket_complain" => $ticketComplain,
+                "statistic" => $countData,
             ];
         } catch (\Throwable $e) {
             $errorMessage = "Internal server error";
