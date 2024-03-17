fe678f484ab356b97d85561daddc35a535661ca5
Apri Jaelani <aprijaelani25@gmail.com>
2024-03-17 09:18:10 +0700

    [UPDATE] 1. perbaiki invoice

--- resources/views/invoice/download.blade.php
+++ resources/views/invoice/download.blade.php
@@ -64,10 +64,10 @@ return $hasil_rupiah;
 </head>
 
 <body>
-    <div class="container" id="printContent">
+    <div class="container" id="printContent" style="padding: 0px">
         <header>
             <div class="row">
-                <table class="table" style="width: 120%">
+                <table class="table" style="width: 112%">
                     <tbody>
                         <tr>
                             <td colspan="3" style="width: 45%; height:100px"><img src="{{public_path('assets/img/header-img.jpg')}}" alt="Kop" width="50%"></td>
