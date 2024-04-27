7a6cd7384a1c5931ed5f1d65a48addeeb0fb9f75
Apri Jaelani <aprijaelani25@gmail.com>
2024-04-27 07:03:38 +0700

    [UPDATE]

--- app/Http/Controllers/VendorInvoiceController.php
+++ app/Http/Controllers/VendorInvoiceController.php
@@ -211,14 +211,23 @@ class VendorInvoiceController extends Controller
             $countVendorInvoice = 0;
             $countVendorInvoicePaid = 0;
 
-            $countVendorInvoice = PurchaseOrder::where("deleted_at", null)->whereBetween("created_at", [$start, $end]);
+            $countVendorInvoice = PurchaseOrder::where("deleted_at", null)->whereBetween("created_at", [$start, $end])->where(function ($query) {
+                $query->where('status', 'like', "%Terkirim%")
+                      ->orWhere('status', 'like', "%Diupload Vendor%")
+                      ->orWhere('status', 'like', "%Diverifikasi Admin%")
+                      ->orWhere('status', 'like', "%Selesai%");
+            });
             if(!is_null($request->vendor_id)){
                 $countVendorInvoice = $countVendorInvoice->where("vendor_id", $request->vendor_id);
             }
             $countVendorInvoice = $countVendorInvoice->count();
             $countVendorInvoicePaid = PurchaseOrder::where("deleted_at", null)->
-                whereBetween("created_at", [$start, $end])->
-                where("status", "like", "%Selesai%");
+                whereBetween("created_at", [$start, $end])->where(function ($query) {
+                $query->where('status', 'like', "%Terkirim%")
+                        ->orWhere('status', 'like', "%Diupload Vendor%")
+                        ->orWhere('status', 'like', "%Diverifikasi Admin%")
+                        ->orWhere('status', 'like', "%Selesai%");
+            });
             if(!is_null($request->vendor_id)){
                 $countVendorInvoicePaid = $countVendorInvoicePaid->where("vendor_id", $request->vendor_id);
             }
